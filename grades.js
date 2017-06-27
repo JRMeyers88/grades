657336da61e9d68f903f021b283e2c92c213b9a1
Joshua Meyers <joshua.r.meyers@gmail.com>
2017-06-27 16:44:31 -0500

    added comments

--- grades.js
+++ grades.js
@@ -39,11 +39,15 @@
 // console.log(scores[11]);
 
 var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87, 100];
+//create grades with empty values to be increased
 var a = 0, b = 0, c = 0, d = 0, f = 0;
 
+//loop to run through array
 for (var i = 0; i < scores.length; i++){
+	//for any item in array (scores[i]) that meet perameter, increase by 1 (#++)
 	if (scores[i] >= 90) {
 		a++;
+	//use "else if" for multiple perameters, end with "else"
 	} else if (scores[i] >= 80 && scores[i] < 90) {
 		b++;
 	} else if (scores[i] >= 70 && scores[i] < 80) {
@@ -54,18 +58,22 @@ for (var i = 0; i < scores.length; i++){
 		f++
 	}
 }
+	//log each grade
 	console.log("a", a);
 	console.log("b", b);
 	console.log("c", c);
 	console.log("d", d);
 	console.log("f", f);
 
+//sort function takes first and second item in array and returns math to determine what is the larger value, workaround for 100 displaying as lesser number
 scores.sort(function(a, b) {
   return a - b;
 });
+//need to console entire function
 console.log("sort", scores.sort(function(a, b){
 	return a - b;
 }));
 console.log("lowest score", scores[0]);
+//length -1 will always display last item in array
 console.log("highest score", scores[scores.length - 1]);
 
